9a678ca4cff17b7615044bc94e87ba80d2010e50
Peter East <peter@repositive.io>
2019-04-21 13:38:17 +0100

    Incremental update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactCard from './components/ContactCard';
+import './style/index.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <div className="hero is-light is-fullheight">
+          <div className="hero-body">
+            <div className="container" style={{position: "fixed", left: "5%"}}>
+              <h1 className="title">Peter East's Personal Internet Dump</h1>
+              <h2 className="subtitle">
+                Fullstack software developer based in <strong>Cambridge</strong>
+                <br/>
+                I believe in <strong>strong types</strong>, functional programming and good <strong>coffee</strong>.
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="container section has-background-white">
+            <h1 className="title is-size-3">Contact me in some places:</h1>
+            <div className="columns">
+              <ContactCard serviceName="LinkedIn" url="https://www.linkedin.com/in/peter-east-a0937754"/>
+              <ContactCard serviceName="GitHub" url="http://www.github.com/petereast"/>
+              <div className="column section">Email</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default (
+  {
+    serviceName,
+    url,
+  }:
+  {
+    serviceName: string;
+    url: string;
+  }
+) => (
+  <div className="column">
+    <div className="shade"></div>
+    <div className="contactCard">
+      <h1 className="title is-size-6">{serviceName}</h1>
+      <a href={url} className="button">View Profile</a>
+    </div>
+  </div>
+)
