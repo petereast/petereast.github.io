25bd0009f8f0c243894bc63ff4ea06b6cf66e949
Peter East <petereast+gpg@pm.me>
2019-06-19 13:31:23 +0100

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import ContactCard from './components/ContactCard';
-import { Helmet } from 'react-helmet';
-import './style/index.scss';
+import React, { Component } from "react";
+import ContactCard from "./components/ContactCard";
+import { Helmet } from "react-helmet";
+import "./style/index.scss";
 
 class App extends Component {
   render() {
@@ -13,22 +13,37 @@ class App extends Component {
         <div className="hero is-light is-fullheight">
           <div className="hero-body">
             <div className="container">
-              <h1 className="title is-light">Peter East's Personal Internet Dump</h1>
+              <h1 className="title is-light">
+                Peter East's Personal Internet Dump
+              </h1>
               <h2 className="subtitle is-size-3">
                 Backend web developer based in <strong>Cambridge</strong>
-                <br/>
-                I believe in <strong>functional programming</strong>, <strong>strong types</strong> and good <strong>coffee</strong>.
+                <br />I believe in <strong>functional programming</strong>,{" "}
+                <strong>strong types</strong> and good <strong>coffee</strong>.
               </h2>
             </div>
           </div>
         </div>
-        <div className="section section--bottom-half">
-          <div className="container section has-background-white">
-            <h1 className="title is-size-3">Contact me in some places:</h1>
-            <div className="columns">
-              <ContactCard serviceName="LinkedIn" url="https://www.linkedin.com/in/peter-east-a0937754"/>
-              <ContactCard serviceName="GitHub" url="http://www.github.com/petereast"/>
-              <ContactCard serviceName="Instagram" url="https://www.instagram.com/petereast_/"/>
+        <div className="hero is-light">
+          <div className="hero-body">
+            <div className="section section--bottom-half">
+              <div className="container section has-background-white">
+                <h1 className="title is-size-3">Contact me in some places:</h1>
+                <div className="columns">
+                  <ContactCard
+                    serviceName="LinkedIn"
+                    url="https://www.linkedin.com/in/peter-east-a0937754"
+                  />
+                  <ContactCard
+                    serviceName="GitHub"
+                    url="http://www.github.com/petereast"
+                  />
+                  <ContactCard
+                    serviceName="Instagram"
+                    url="https://www.instagram.com/petereast_/"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
