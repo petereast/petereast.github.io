e27c90b6f814c5ef73b8745e93507a5021ad28e8
Peter East <petereast+gpg@pm.me>
2019-11-26 18:51:33 +0000

    Make everything smaller

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -3,15 +3,9 @@ import * as React from "react";
 const current = () => (
   <div className="site-header section has-background-white">
     <h2 className="title is-size-4 ">
-      <span className="icon is-small margin-right-12">
-        <i className="fa fa-arrow-right" />
-      </span>
       Peter East Software Consultancy
     </h2>
     <h2 className="subtitle is-size-5">
-      <span className="icon is-small margin-right-12">
-        <i className="fa fa-arrow-right" />
-      </span>
       hello<i className="fa fa-at" aria-hidden="true" />peterea.st
     </h2>
   </div>
