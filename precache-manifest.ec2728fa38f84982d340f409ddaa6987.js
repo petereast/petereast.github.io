d3d6ecbda8a7e4ae3964899dfa728038c22e5f12
Peter East <petereast+gpg@pm.me>
2019-11-26 18:47:54 +0000

    Updates

---
+++ precache-manifest.ec2728fa38f84982d340f409ddaa6987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad3caac0081ba7c909a2",
+    "url": "/static/js/main.bfae56b8.chunk.js"
+  },
+  {
+    "revision": "40debd9ea155314160a0",
+    "url": "/static/js/2.b579ad18.chunk.js"
+  },
+  {
+    "revision": "ad3caac0081ba7c909a2",
+    "url": "/static/css/main.af47f29a.chunk.css"
+  },
+  {
+    "revision": "fd1e626822c290b9ad56d910d7e4d45e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98df20db13e2b31847087ca7d7962565.js"
+  "/precache-manifest.ec2728fa38f84982d340f409ddaa6987.js"
 );
 
 workbox.clientsClaim();
